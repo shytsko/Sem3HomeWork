2897f04773aac6b0c580b396b26bdae5d7226ddf
Vladimir Shytsko <shytsko.v@gmail.com>
2022-04-19 14:35:16 +0300

    Add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// **Задача 14**. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+int number = GetInteger();
+
+string result;
+if(IsМultiples(number, 7) && IsМultiples(number, 23))
+    result = "кратно";
+else
+    result = "не кратно";
+
+Console.WriteLine($"Число {number} {result} одновременно 7 и 23");
+
+int GetInteger()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool IsМultiples(int num1, int num2)
+{
+    return num1 % num2 == 0;
+}
