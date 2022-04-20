106e1ca298e4d3d1d006cbcc98aa15ca7e70daeb
Vladimir Shytsko <shytsko.v@gmail.com>
2022-04-20 09:37:45 +0300

    Add Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,10 @@
+﻿// Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
+
+Console.WriteLine("Введите что угодно. для выхода введите exit");
+while(true)
+{
+    Console.Write("> ");
+    string inputString = Console.ReadLine();
+    if(inputString == "exit" || inputString == "Exit")
+        break;
+}
