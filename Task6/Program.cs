25a048e5f73a3c07ebde1e78c0bc9509aa77e3ac
Vladimir Shytsko <shytsko.v@gmail.com>
2022-04-20 09:31:53 +0300

    Add Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,33 @@
+﻿// В переменной string есть секретное сообщение, во второй есть пароль.
+// Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.
+
+string secretMessage = "Секретное сообщение";
+
+string password = "пароль";
+
+Console.WriteLine("Введите пароль, чтобы прочитать секретное сообщение.");
+
+int countTries = 3;
+
+while (countTries > 0)
+{
+    Console.Write("> ");
+    string inputPsw = Console.ReadLine();
+    if (CheckPassword(password, inputPsw))
+        break;
+    else
+    {
+        Console.WriteLine("Пароль не верный.");
+        countTries--;
+    }
+}
+
+if (countTries > 0)
+    Console.WriteLine(secretMessage);
+else
+    Console.WriteLine("Вы исчерпали допустимое количество попыток.");
+
+bool CheckPassword(string psw, string userPsw)
+{
+    return userPsw == psw;
+}
